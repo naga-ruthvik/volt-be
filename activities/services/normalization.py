b369e230aa3d4f2c3ba53e04ccad50a79fc32862
Naga Ruthvik <kondenagaruthvik@gmail.com>
2026-04-23 22:33:39 +0530

    create: codeforces API integration

--- activities/services/normalization.py
+++ activities/services/normalization.py
@@ -1,8 +1,28 @@
 # normalize all the response we get from platforms APIs
+from datetime import datetime, timezone
+
+
 class ActivityNormalization:
     def github_activity_normalizer(self, events):
         activity_map = {}
         for event in events:
             date = event["created_at"][:10]
             activity_map[date] = activity_map.get(date, 0) + 1
+        return activity_map
+
+    def codeforces_activity_normalizer(self, events):
+        activity_map = {}
+
+        for event in events.get("result", []):
+            timestamp = event.get("creationTimeSeconds")
+
+            if not timestamp:
+                continue
+
+            date = datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime(
+                "%Y-%m-%d"
+            )
+
+            activity_map[date] = activity_map.get(date, 0) + 1
+
         return activity_map
--- activities/services/sync_service.py
+++ activities/services/sync_service.py
@@ -7,20 +7,28 @@ from activities.models import Activity, GenerationRequest, Platform, PlatformAcc
 from activities.services.activity_service import ActivityService
 from activities.services.metrics_service import MetricsService
 from activities.services.normalization import ActivityNormalization
+from activities.services.platforms.codeforces_service import CodeforcesService
 from activities.services.platforms.github_service import GitHubService
 
 from .activity_service import ActivityService
 from .metrics_service import MetricsService
+from .test.normalization_test import normalizer
 
 
 class SyncService:
     @staticmethod
     def sync_github_data(username):
         github_service = GitHubService()
-        normalization_service = ActivityNormalization()
-
+        normalizer = ActivityNormalization()
         events = github_service.fetch_events(username)
-        return normalization_service.github_activity_normalizer(events)
+        return normalizer.github_activity_normalizer(events)
+
+    @staticmethod
+    def sync_codeforces_data(username):
+        codeforces_service = CodeforcesService()
+        events = codeforces_service.fetch_activities(username)
+        normalizer = ActivityNormalization()
+        return normalizer.codeforces_activity_normalizer(events)
 
     @staticmethod
     def sync_all_platforms(generation_request):
@@ -29,7 +37,7 @@ class SyncService:
         platform_accounts = PlatformAccount.objects.filter(user=user)
 
         all_data = []
-
+        # TODO: include all the platforms for syncing
         for account in platform_accounts:
             if account.platform == Platform.GITHUB:
                 try:
@@ -39,7 +47,14 @@ class SyncService:
                     PlatformAccount.objects.filter(id=account.id).update(
                         last_fetched=timezone.now(), fetch_error=str(e)
                     )
-
+            if account.platform == Platform.CODEFORCES:
+                try:
+                    data = SyncService.sync_codeforces_data(account.username)
+                    all_data.append((account, data))
+                except Exception as e:
+                    PlatformAccount.objects.filter(id=account.id).update(
+                        last_fetched=timezone.now(), fetch_error=str(e)
+                    )
         with transaction.atomic():
             for account, normalized_events in all_data:
                 ActivityService.bulk_save(
