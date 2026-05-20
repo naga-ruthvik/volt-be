c4ceecb88e6359eb93d9a9fab744271a263382dc
Naga Ruthvik <kondenagaruthvik@gmail.com>
2026-05-20 15:55:10 +0530

    update: admin site

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import OTPSessions
+from .models import OTPSessions, User
 
 admin.site.register(OTPSessions)
+admin.site.register(User)
